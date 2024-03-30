bae252be8801494779af78fd683b2d26c3acb289
Ravish Bisht <ravishbisht85@gmail.com>
2024-03-30 11:01:44 +0530

    initialized next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "music school",
+  description: "learn the sound of inner soul",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <h1 className="text-5xl text-center text-blue-300">ravish aur me</h1>;
+}
