a14a283542b7d603ee152e6c11780dfc292e32f7
Ravish Bisht <ravishbisht85@gmail.com>
2024-03-31 11:33:46 +0530

    courses and conatct route add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@ import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
-      <FeaturedCourses/>
-      <WhyChooseUs/>
-      <MusicSchoolTestimonials/>
-      <UpcomingWebinars/>
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
